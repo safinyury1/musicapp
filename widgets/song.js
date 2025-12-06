87fe9d2b570f58a26c8cb4f8eca6089772457f7e
safinyury1 <safin15092005@mail.ru>
2025-12-06 18:45:34 +0500

    музыкальный плеер готов

--- widgets/song.js
+++ widgets/song.js
@@ -38,7 +38,7 @@ export const Song = (track) => {
             </div>
             <div className="flex items-center gap-3">
                 <button onClick={() => handleChangeLike()}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill={like ? '#86C232' : 'gray'} viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill={like ? '#d81661ff' : 'gray'} viewBox="0 0 16 16">
                         <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
                     </svg>
                 </button>
